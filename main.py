f1e0ab82a4d891f329791c18c9aa0ff870c139e6
Mathieu <78476132+MathieuTOUDIC@users.noreply.github.com>
2024-08-01 14:18:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from nom_du_fichier import Ui_MainWindow  # Assurez-vous que le nom de la classe correspond à celui généré par pyuic5
+
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
