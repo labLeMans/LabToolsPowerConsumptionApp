ebae7c8ca367cf076be1adaa471f2705c27f3971
Mathieu <78476132+MathieuTOUDIC@users.noreply.github.com>
2024-08-05 11:18:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.openButton.clicked.connect(self.show_second_window)
+        self.pushButton.clicked.connect(self.show_second_window)
 
     def show_second_window(self):
-        self.second_window = SecondApp()
+        tool_button_states = self.get_tool_button_states()
+        self.second_window = SecondApp(tool_button_states)
         self.second_window.show()
+        self.close() # ferme la fenetre principale
+
+    def get_tool_button_states(self):
+        #récupération des checkbox full power et low battery
+        states = {
+            'fullpower': self.FullPowercheckBox.isChecked(),
+            'lowbattery': self.LowBatterycheckBox.isChecked()
+        
+        }
+        return states
 
 class SecondApp(QMainWindow):
-    def __init__(self):
+    def __init__(self, tool_button_states):
         super().__init__()
         uic.loadUi('second.ui', self)
+        self.display_tool_button_states(tool_button_states)
+
+    def display_tool_button_states(self, states):
+        # Affiche les états de full power et low battery
+        for button, state in state.items():
+            print(f'{button} is {"checked" if state else "unchecked"}')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
