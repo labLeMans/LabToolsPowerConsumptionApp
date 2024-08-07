869a410914a45a0b824accc53e5975e678446084
Mathieu <78476132+MathieuTOUDIC@users.noreply.github.com>
2024-08-07 14:37:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,138 +10,40 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
 class MainApp(QMainWindow):
+    power_updated = pyqtSignal(float)  # Signal personnalisé pour mettre à jour la puissance
     def __init__(self):
         super().__init__()
         self.load_ui()
         self.setup_connections()
+        self.max_power = 0  # Initialiser la puissance maximale
+        self.collecting_data = False  # Flag to check if data collection is ongoing
+        self.power_data = {}  # Dictionnaire pour stocker la puissance maximale pour chaque mode
+        #self.modulename = modulename # Stocker le nom du module
+        self.selected_item = "OFF"
+        self.power_updated.connect(self.update_power_display)# Connecter le signal power_updated à la méthode update_power_display
+
 
     def load_ui(self):
         """Charge le fichier UI pour la fenêtre principale."""
-        uic.loadUi('main.ui', self)
+        uic.loadUi('wind.ui', self)
 
     def setup_connections(self):
         """Configure les connexions des signaux et slots."""
-        self.validatePushButton.clicked.connect(self.show_second_window)
+        #self.manualSwitchCheckBox.clicked.connect()
+        #self.ignitionCheckBox.clicked.connect()
+        #self.fullPowerCheckBox.clicked.connect()
+        #self.lowBatteryCheckBox.clicked.connect()
+        self.reportPushButton.clicked.connect(self.module_identification)
 
-    def show_second_window(self):
-        """Crée et affiche la fenêtre secondaire, puis ferme la fenêtre principale."""
+    def module_identification(self, modulename):
+        """Vérification si le nom du module à bien été renseigné."""
         modulename = self.moduleNameLineEdit.text().strip() # Récupère le nom du module dans la QLineEdit
-        
         if not modulename:
             # Affiche un message d'erreur si la QLineEdit est vide
             QMessageBox.warning(self, "Nom du module manquant", "Veuillez entrer le nom du module")
             return # Empêche le passage à la fenêtre suivante
-        
-        tool_button_states = self.get_tool_button_states()
-        modulename = self.moduleNameLineEdit.text()  # Récupère le nom du fichier depuis le QLineEdit
-        self.second_window = SecondApp(tool_button_states, modulename)
-        self.second_window.show()
-        self.close()  # Ferme la fenêtre principale
-
-    def get_tool_button_states(self):
-        """Récupère les états des CheckBox dans la fenêtre principale."""
-        states = {
-            'fullpower': self.FullPowercheckBox.isChecked(),
-            'lowbattery': self.LowBatterycheckBox.isChecked()
-        }
-        return states
-
-class SecondApp(QMainWindow):
-    power_updated = pyqtSignal(float)  # Signal personnalisé pour mettre à jour la puissance
-
-    def __init__(self, tool_button_states, modulename):
-        super().__init__()
-        self.load_ui()
-        self.display_tool_button_states(tool_button_states)
-        self.setup_connections()
-        self.max_power = 0  # Initialiser la puissance maximale
-        self.collecting_data = False  # Flag to check if data collection is ongoing
-        self.power_data = {}  # Dictionnaire pour stocker la puissance maximale pour chaque mode
-        self.modulename = modulename # Stocker le nom du module
-        self.selected_item = "OFF"
-
-        # Connecter le signal power_updated à la méthode update_power_display
-        self.power_updated.connect(self.update_power_display)
-
-    def load_ui(self):
-        """Charge le fichier UI pour la fenêtre secondaire."""
-        uic.loadUi('second.ui', self)
-
-    def setup_connections(self):
-        """Configure les connexions des signaux et slots pour la fenêtre secondaire."""
-        self.startPushButton.clicked.connect(self.start_collecting)
-        self.stopPushButton.clicked.connect(self.stop_collecting)
-        self.endPushButton.clicked.connect(self.generate_excel)  # Connexion pour le bouton "End"
-        self.ecoModecomboBox.currentIndexChanged.connect(self.save_selected_item)
-
-    def display_tool_button_states(self, states):
-        """Met à jour les éléments de la fenêtre secondaire en fonction des états des CheckBox."""
-        self.update_light(self.fullPowerLight, states['fullpower'])
-        self.update_light(self.lowBatteryLight, states['lowbattery'])
-        self.update_ecoModecomboBox(states)
-
-    def update_light(self, label, state):
-        """Met à jour l'image du label en fonction de l'état (vert pour actif, rouge pour inactif)."""
-        if state:
-            label.setPixmap(QtGui.QPixmap('image/green_light.png'))
         else:
-            label.setPixmap(QtGui.QPixmap('image/red_light.png'))
-
-    def update_ecoModecomboBox(self, states):
-        """Met à jour les éléments de la ComboBox en fonction des états des CheckBox."""
-        self.ecoModecomboBox.clear()  # Vide la ComboBox avant de la mettre à jour
-        
-        if states['fullpower']:
-            self.add_fullpower_items(states['lowbattery'])
-        else:
-            self.add_no_fullpower_items(states['lowbattery'])
-
-    def add_fullpower_items(self, lowbattery):
-        """Ajoute les éléments de la ComboBox lorsque le mode 'fullpower' est activé."""
-        self.ecoModecomboBox.addItem("OFF")
-        self.ecoModecomboBox.addItem("Sleep")
-        self.ecoModecomboBox.addItem("ECO 0")
-        self.ecoModecomboBox.addItem("ECO 1")
-        self.ecoModecomboBox.addItem("ECO 2")
-        if lowbattery:
-            self.ecoModecomboBox.addItem("Low Battery")
-
-    def add_no_fullpower_items(self, lowbattery):
-        """Ajoute les éléments de la ComboBox lorsque le mode 'fullpower' est désactivé."""
-        self.ecoModecomboBox.addItem("OFF")
-        self.ecoModecomboBox.addItem("Sleep")
-        self.ecoModecomboBox.addItem("ECO 0")
-        if lowbattery:
-            self.ecoModecomboBox.addItem("Low Battery")
-
-    def save_selected_item(self):
-        """Sauvegarde l'élément sélectionné dans la ComboBox."""
-        self.selected_item = self.ecoModecomboBox.currentText()
-
-    def start_collecting(self):
-        """Démarre la collecte des données de puissance."""
-        self.max_power = 0  # Réinitialiser la puissance maximale
-        self.collecting_data = True
-        self.collect_data_thread = threading.Thread(target=self.collect_data)
-        self.collect_data_thread.start()
-
-    def stop_collecting(self):
-        """Arrête la collecte des données de puissance."""
-        self.collecting_data = False
-        if self.collect_data_thread.is_alive():
-            self.collect_data_thread.join()
-        self.selectedItemLabel.setText(f"Max Power: {self.max_power:.2f} W")
-        if self.selected_item:
-            self.power_data[self.selected_item] = self.max_power  # Enregistre la puissance maximale pour le mode actuel
-
-    def collect_data(self):
-        """Collecte les données de puissance à intervalle régulier."""
-        while self.collecting_data:
-            power = self.fetch_power_value()
-            if power is not None:
-                self.max_power = max(self.max_power, power)
-                self.power_updated.emit(self.max_power)  # Émettre le signal de mise à jour
-            time.sleep(1)  # Attendre 1 seconde entre les collectes
+            self.generate_excel
 
     def fetch_power_value(self):
         """Récupère la valeur de puissance actuelle depuis l'URL."""
